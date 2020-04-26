bbe6157bf7fc15a7a0ccef1b5cbd0eb0277493d7
Sidney Pimentel <sidneyspe@gmail.com>
2020-04-26 00:49:57 -0300

    init project

---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  body{
+    background: #312E38;
+    color: #FFF;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button{
+    font-family: 'Roboto Slab', sans-serif;
+    font-size: 16px;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong{
+    font-weight: 500;
+  }
+
+  button{
+    cursor: pointer
+  }
+
+  @-webkit-keyframes autofill {
+    0%,100% {
+        color: #FFF;
+        font-size: 16px;
+        background: transparent;
+    }
+  }
+
+  input:-webkit-autofill {
+      -webkit-animation-delay: 1s; /* Safari support - any positive time runs instantly */
+      -webkit-animation-name: autofill;
+      -webkit-animation-fill-mode: both;
+  }
+
+`;
